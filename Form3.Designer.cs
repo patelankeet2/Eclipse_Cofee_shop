6ad406564c1e53f8e58d0bd97b9c1d2918ddb298
shubhamkataria2005 <katariashubham0101@gmail.com>
2024-11-06 09:57:15 +1300

    Added Basic Design of dashboard - Shubham

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -28,12 +28,260 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel17 = new System.Windows.Forms.Panel();
+            this.panel18 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.dashboard = new System.Windows.Forms.Label();
+            this.panel19 = new System.Windows.Forms.Panel();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.logOut = new System.Windows.Forms.Button();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.tableReservation = new System.Windows.Forms.Button();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.order = new System.Windows.Forms.Button();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.pictureBox15 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.card = new System.Windows.Forms.Button();
+            this.profile = new System.Windows.Forms.Button();
+            this.panel4.SuspendLayout();
+            this.panel17.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel19.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Blue;
+            this.panel4.Controls.Add(this.panel17);
+            this.panel4.Controls.Add(this.dashboard);
+            this.panel4.Location = new System.Drawing.Point(12, 12);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(213, 68);
+            this.panel4.TabIndex = 20;
+            // 
+            // panel17
+            // 
+            this.panel17.BackColor = System.Drawing.Color.Transparent;
+            this.panel17.Controls.Add(this.panel18);
+            this.panel17.Controls.Add(this.pictureBox4);
+            this.panel17.Location = new System.Drawing.Point(199, 51);
+            this.panel17.Name = "panel17";
+            this.panel17.Size = new System.Drawing.Size(732, 461);
+            this.panel17.TabIndex = 8;
+            // 
+            // panel18
+            // 
+            this.panel18.BackColor = System.Drawing.Color.White;
+            this.panel18.Location = new System.Drawing.Point(13, 20);
+            this.panel18.Name = "panel18";
+            this.panel18.Size = new System.Drawing.Size(200, 100);
+            this.panel18.TabIndex = 0;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(-378, -32);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(349, 548);
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // dashboard
+            // 
+            this.dashboard.AutoSize = true;
+            this.dashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboard.ForeColor = System.Drawing.Color.White;
+            this.dashboard.Location = new System.Drawing.Point(18, 15);
+            this.dashboard.Name = "dashboard";
+            this.dashboard.Size = new System.Drawing.Size(140, 29);
+            this.dashboard.TabIndex = 1;
+            this.dashboard.Text = "Dashboard";
+            // 
+            // panel19
+            // 
+            this.panel19.BackColor = System.Drawing.Color.White;
+            this.panel19.Controls.Add(this.pictureBox5);
+            this.panel19.Controls.Add(this.logOut);
+            this.panel19.Controls.Add(this.pictureBox6);
+            this.panel19.Controls.Add(this.tableReservation);
+            this.panel19.Controls.Add(this.pictureBox7);
+            this.panel19.Controls.Add(this.order);
+            this.panel19.Controls.Add(this.pictureBox14);
+            this.panel19.Controls.Add(this.pictureBox15);
+            this.panel19.Controls.Add(this.button1);
+            this.panel19.Controls.Add(this.card);
+            this.panel19.Controls.Add(this.profile);
+            this.panel19.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel19.Location = new System.Drawing.Point(12, 12);
+            this.panel19.Name = "panel19";
+            this.panel19.Size = new System.Drawing.Size(213, 543);
+            this.panel19.TabIndex = 19;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(0, 255);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(28, 32);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            // 
+            // logOut
+            // 
+            this.logOut.BackColor = System.Drawing.Color.Yellow;
+            this.logOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logOut.Location = new System.Drawing.Point(23, 492);
+            this.logOut.Name = "logOut";
+            this.logOut.Size = new System.Drawing.Size(155, 36);
+            this.logOut.TabIndex = 5;
+            this.logOut.Text = "Log Out";
+            this.logOut.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(0, 216);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(28, 33);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            // 
+            // tableReservation
+            // 
+            this.tableReservation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableReservation.ForeColor = System.Drawing.Color.Blue;
+            this.tableReservation.Location = new System.Drawing.Point(0, 255);
+            this.tableReservation.Name = "tableReservation";
+            this.tableReservation.Size = new System.Drawing.Size(213, 32);
+            this.tableReservation.TabIndex = 5;
+            this.tableReservation.Text = "      Table Reservation";
+            this.tableReservation.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.tableReservation.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(0, 177);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(28, 33);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            // 
+            // order
+            // 
+            this.order.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.order.ForeColor = System.Drawing.Color.Blue;
+            this.order.Location = new System.Drawing.Point(0, 216);
+            this.order.Name = "order";
+            this.order.Size = new System.Drawing.Size(213, 33);
+            this.order.TabIndex = 4;
+            this.order.Text = "      Order";
+            this.order.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.order.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.Location = new System.Drawing.Point(0, 138);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(28, 33);
+            this.pictureBox14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox14.TabIndex = 6;
+            this.pictureBox14.TabStop = false;
+            // 
+            // pictureBox15
+            // 
+            this.pictureBox15.Location = new System.Drawing.Point(0, 100);
+            this.pictureBox15.Name = "pictureBox15";
+            this.pictureBox15.Size = new System.Drawing.Size(28, 34);
+            this.pictureBox15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox15.TabIndex = 1;
+            this.pictureBox15.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(0, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "      Menu";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // card
+            // 
+            this.card.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.card.ForeColor = System.Drawing.Color.Blue;
+            this.card.Location = new System.Drawing.Point(0, 177);
+            this.card.Name = "card";
+            this.card.Size = new System.Drawing.Size(213, 33);
+            this.card.TabIndex = 3;
+            this.card.Text = "      Cart";
+            this.card.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.card.UseVisualStyleBackColor = true;
+            // 
+            // profile
+            // 
+            this.profile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.profile.ForeColor = System.Drawing.Color.Blue;
+            this.profile.Location = new System.Drawing.Point(0, 100);
+            this.profile.Name = "profile";
+            this.profile.Size = new System.Drawing.Size(213, 34);
+            this.profile.TabIndex = 0;
+            this.profile.Text = "      Profile";
+            this.profile.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.profile.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::Eclipse_Cofee_shop.Properties.Resources.dashboard_background_img;
+            this.ClientSize = new System.Drawing.Size(841, 567);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel19);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel17.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel19.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel17;
+        private System.Windows.Forms.Panel panel18;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label dashboard;
+        private System.Windows.Forms.Panel panel19;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button logOut;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Button tableReservation;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Button order;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.PictureBox pictureBox15;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button card;
+        private System.Windows.Forms.Button profile;
     }
 }
