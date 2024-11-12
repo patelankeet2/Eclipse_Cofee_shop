d2f1bcd37b80a9653e507430e85552416b24806a
kawal285 <kawalpreetkaur285@gmail.com>
2024-11-13 10:18:13 +1300

    Updated design of Register Page ~ Kawal

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -32,18 +32,20 @@
             this.buttonRegister = new System.Windows.Forms.Button();
             this.textBoxregUsername = new System.Windows.Forms.TextBox();
             this.textBoxregPassword = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonRegister
             // 
-            this.buttonRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonRegister.ForeColor = System.Drawing.Color.Maroon;
-            this.buttonRegister.Location = new System.Drawing.Point(349, 319);
+            this.buttonRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRegister.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.buttonRegister.Location = new System.Drawing.Point(352, 321);
             this.buttonRegister.Name = "buttonRegister";
-            this.buttonRegister.Size = new System.Drawing.Size(107, 43);
+            this.buttonRegister.Size = new System.Drawing.Size(127, 42);
             this.buttonRegister.TabIndex = 0;
             this.buttonRegister.Text = "Register";
             this.buttonRegister.UseVisualStyleBackColor = true;
@@ -51,43 +53,61 @@
             // 
             // textBoxregUsername
             // 
-            this.textBoxregUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxregUsername.ForeColor = System.Drawing.Color.Maroon;
-            this.textBoxregUsername.Location = new System.Drawing.Point(349, 207);
+            this.textBoxregUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxregUsername.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.textBoxregUsername.Location = new System.Drawing.Point(319, 222);
             this.textBoxregUsername.Name = "textBoxregUsername";
-            this.textBoxregUsername.Size = new System.Drawing.Size(107, 26);
+            this.textBoxregUsername.Size = new System.Drawing.Size(184, 26);
             this.textBoxregUsername.TabIndex = 1;
             this.textBoxregUsername.Text = "Username";
+            this.textBoxregUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // textBoxregPassword
             // 
-            this.textBoxregPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxregPassword.ForeColor = System.Drawing.Color.Maroon;
-            this.textBoxregPassword.Location = new System.Drawing.Point(349, 262);
+            this.textBoxregPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxregPassword.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.textBoxregPassword.Location = new System.Drawing.Point(319, 275);
             this.textBoxregPassword.Name = "textBoxregPassword";
-            this.textBoxregPassword.Size = new System.Drawing.Size(107, 26);
+            this.textBoxregPassword.Size = new System.Drawing.Size(184, 26);
             this.textBoxregPassword.TabIndex = 2;
             this.textBoxregPassword.Text = "Password";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.ForeColor = System.Drawing.Color.Maroon;
-            this.textBox3.Location = new System.Drawing.Point(277, 389);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(321, 26);
-            this.textBox3.TabIndex = 3;
-            this.textBox3.Text = "Already have an account ? Login Here";
+            this.textBoxregPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(294, 27);
+            this.pictureBox1.Location = new System.Drawing.Point(229, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(219, 144);
+            this.pictureBox1.Size = new System.Drawing.Size(339, 154);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(295, 392);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Already  have any account?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label3.Location = new System.Drawing.Point(473, 392);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Login here.";
             // 
             // Form2
             // 
@@ -96,8 +116,9 @@
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBoxregPassword);
             this.Controls.Add(this.textBoxregUsername);
             this.Controls.Add(this.buttonRegister);
@@ -115,7 +136,8 @@
         private System.Windows.Forms.Button buttonRegister;
         private System.Windows.Forms.TextBox textBoxregUsername;
         private System.Windows.Forms.TextBox textBoxregPassword;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
     }
 }
