926537f8b015c90a5010893e9ae6e603d5b7d1e2
shubhamkataria2005 <katariashubham0101@gmail.com>
2024-11-14 20:44:09 +1300

    added profile page into dashboard ~ Shubham

--- Profile.cs
+++ Profile.cs
@@ -16,5 +16,31 @@ namespace Eclipse_Cofee_shop
         {
             InitializeComponent();
         }
+
+        private void profile1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menu_Click(object sender, EventArgs e)
+        {
+            menu menu = new menu();
+            menu.Show(this);
+            this.Hide();
+        }
+
+        private void card_Click(object sender, EventArgs e)
+        {
+            cart cart = new cart();
+            cart.Show(this);
+            this.Hide();
+        }
+
+        private void viewCard_Click(object sender, EventArgs e)
+        {
+            menu menu = new menu();
+            menu.Show(this);
+            this.Hide();
+        }
     }
 }
