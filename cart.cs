410f712ec54c0e435146cb6bf783aa3d80fc6190
patelankeet2 <patelankeet2@gmail.com>
2024-11-25 22:53:10 +1300

    updated code for Cart & connected with dbItem ~ Ankeet

--- cart.cs
+++ cart.cs
@@ -37,5 +37,51 @@ namespace Eclipse_Cofee_shop
             payment.Show(this);
             this.Hide();
         }
+
+        private void logOut_Click(object sender, EventArgs e)
+        {
+            Form1 LoginPage = new Form1();
+            LoginPage.Show();
+            this.Hide();
+        }
+
+        private void LoadCartData()
+        {
+            try
+            {
+                dbItem db = new dbItem();
+                string[] itemIds = { "1", "2", "3", "4" }; 
+
+                dataGridView1.Rows.Clear();
+
+                foreach (var itemId in itemIds)
+                {
+                    if (db.ReadItem(itemId))
+                    {
+                        dataGridView1.Rows.Add(
+                            db.data.Item,
+                            db.data.Quantity,
+                            db.data.Price.ToString("C"),
+                            db.data.Total.ToString("C"),
+                            "Edit",
+                            "Delete"
+                        );
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Error loading item ID {itemId}: {db.LastError}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while loading the cart: {ex.Message}");
+            }
+        }
+
+        private void cart_Load(object sender, EventArgs e)
+        {
+            LoadCartData();
+        }
     }
 }
