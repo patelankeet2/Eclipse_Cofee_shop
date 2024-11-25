e3b6a5898d3ee03a51bc0c82a056fb2a537fac3b
kawal285 <kawalpreetkaur285@gmail.com>
2024-11-25 13:20:06 +1300

    Add input resriction for different fields ~ kawal

--- Payment.cs
+++ Payment.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -77,6 +78,75 @@ namespace Eclipse_Cofee_shop
                 }
             };
         }
+
+        private void AddInputRestrictions()
+        {
+            // Format Account Number as NNNN-NNNN-NNNN-NNNN
+            textBoxaccnum.KeyPress += (sender, e) =>
+            {
+                // Allow only digits and backspace
+                if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                    return;
+                }
+
+                if (e.KeyChar == (char)Keys.Back) // Allow backspace
+                {
+                    return;
+                }
+
+                string rawInput = textBoxaccnum.Text.Replace("-", ""); // Remove dashes to count digits
+
+                if (rawInput.Length >= 16) // Limit to 16 digits
+                {
+                    e.Handled = true;
+                    return;
+                }
+
+                // Add dashes after every 4 digits
+                if (rawInput.Length % 4 == 0 && rawInput.Length > 0)
+                {
+                    textBoxaccnum.Text += "-";
+                    textBoxaccnum.SelectionStart = textBoxaccnum.Text.Length; // Set cursor position
+                }
+            };
+
+            // Restrict CVC Code to 3 digits
+            textBoxSecCode.KeyPress += (sender, e) =>
+            {
+                if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back) // Allow digits and backspace
+                {
+                    e.Handled = true;
+                }
+
+                if (textBoxSecCode.Text.Length >= 3 && e.KeyChar != (char)Keys.Back) // Limit to 3 digits
+                {
+                    e.Handled = true;
+                }
+            };
+
+            // Validate Expiry Date format as MM/YY
+            textBoxExpDate.TextChanged += (sender, e) =>
+            {
+                if (textBoxExpDate.Text.Length == 5) // Check for "MM/YY" length
+                {
+                    if (!DateTime.TryParseExact(textBoxExpDate.Text, "MM/yy", CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
+                    {
+                        textBoxExpDate.ForeColor = Color.Red; // Indicate invalid format
+                    }
+                    else
+                    {
+                        textBoxExpDate.ForeColor = Color.Black; // Valid format
+                    }
+                }
+                else
+                {
+                    textBoxExpDate.ForeColor = Color.Gray; // Placeholder or incomplete
+                }
+            };
+        }
+
 
 
         private void buttonSubmitPayment_Click(object sender, EventArgs e)
